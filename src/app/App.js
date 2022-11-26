51e0b301926ec6bb577a14cfd019a1b5edba3ebc
Filippo <filippo.mazzalai@gmail.com>
2022-11-26 13:10:20 +0100

    moved router inside index.js

--- src/app/App.js
+++ src/app/App.js
@@ -1,54 +1,61 @@
 import React, { useState } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-import socketIO from 'socket.io-client';
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { io } from 'socket.io-client';
 
 import PrivateRoute from "../components/PrivateRoute";
 import Login from "../components/Login";
 import Home from "../components/Home";
 import Chat from "../components/Chat";
 
-const socket = socketIO.connect('http://localhost:4000');
+const socket = io('http://localhost:4000');
 
 export default function App() {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [userName, setUserName] = useState("");
+    const navigate = useNavigate();
 
     function submitLogin(event) {
         event.preventDefault();
         if (userName) {
             setIsLoggedIn(true);
         }
+        navigate("/home");
+    }
+
+    function logout(event) {
+        event.preventDefault();
+        setUserName("");
+        setIsLoggedIn(false);
+        navigate("/");
     }
 
     return (
-        <div className="app-container">
-            <BrowserRouter>
-                <Route 
-                    path="/" 
-                    element={
-                        <Login 
-                            handleSubmit={submitLogin}
-                            setUserName={setUserName} />
-                    } />
+        <Routes>
+            <Route 
+                path="/" 
+                element={
+                    <Login 
+                        handleSubmit={submitLogin}
+                        setUserName={setUserName} />
+                } />
 
-                <Route 
-                    path="/home" 
-                    element={
-                        <PrivateRoute isLoggedIn={isLoggedIn}>
-                            <Home 
-                                userName={userName} />
-                        </PrivateRoute>
-                    } />
+            <Route 
+                path="/home" 
+                element={
+                    <PrivateRoute isLoggedIn={isLoggedIn}>
+                        <Home 
+                            userName={userName} 
+                            handleLogout={logout} />
+                    </PrivateRoute>
+                } />
 
-                <Route 
-                    path="/chat/:id"
-                    element={
-                        <PrivateRoute isLoggedIn={isLoggedIn}>
-                            <Chat />
-                        </PrivateRoute>
-                    } />
-
-            </BrowserRouter>
-        </div>
+            <Route 
+                path="/chat/:id"
+                element={
+                    <PrivateRoute isLoggedIn={isLoggedIn}>
+                        <Chat socket={socket} />
+                    </PrivateRoute>
+                } />
+        </Routes>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./app/App";
@@ -8,6 +9,8 @@ const rootElement = createRoot(document.getElementById("root"));
 
 rootElement.render(
     <StrictMode>
-        <App />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </StrictMode>
 )
