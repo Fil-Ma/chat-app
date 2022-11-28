60b1265d02d89b01eb7332bc67835753b57ce737
Filippo <filippo.mazzalai@gmail.com>
2022-11-28 11:27:05 +0100

    added use params hook to manage room id

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -1,17 +1,18 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+
 import ChatBody from "./ChatBody";
 import ChatTools from "./ChatTools";
 import MessageEditor from "./MessageEditor";
 
 export default function ChatRoom({
     roomMessages,
-    room,
     users,
     handleSubmit,
     newMessage,
     setNewMessage
 }) {
-    
+    const { room } = useParams();
     return (
         <div className="chat-container">
             <div className="chat-body">
