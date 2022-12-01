f5d5889a38de84e92a07bc73732b30278846bc15
Filippo <filippo.mazzalai@gmail.com>
2022-12-01 17:01:07 +0100

    styled notFoundPage, added 404 image

--- src/components/not-found-page/NotFound.js
+++ src/components/not-found-page/NotFound.js
@@ -1,10 +1,21 @@
+import "./not-found.css";
 import React from "react";
+import { Link } from "react-router-dom";
+import notFoundImage from "../../resources/404_light.png";
 
 export default function NotFound() {
 
     return (
-        <div>
-            <h1>Page not found</h1>
-        </div>
+        <div className="page-not-found">
+            <div className="error-message">
+                <h1>Whoops..</h1>
+                <p>It looks like the page you are looking for does not exists.
+                    Try again from the <Link to="/home" className="homepage-link" >homepage</Link></p>
+            </div>
+
+            <div className="error-code">
+                <img src={notFoundImage} alt="Page not found error image"/>
+            </div>
+      </div>
     )
 }
