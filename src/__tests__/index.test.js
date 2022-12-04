bf5884564ac78a76d31adf1a0bc9db65c005824f
Filippo <filippo.mazzalai@gmail.com>
2022-12-04 16:10:04 +0100

    changed test file name, test now passing

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -3,10 +3,13 @@ import { BrowserRouter } from "react-router-dom";
 import { render, screen } from "@testing-library/react";
 
 import App from "../app/App";
+import Login from "../components/login/Login";
+
+import LanguageContext from "../languages";
 
 describe("App function component", () => {
 
-    it("Renders without errors", () => {
+    it("renders without errors", () => {
         render(<App />, { wrapper: BrowserRouter });
     }) 
     
